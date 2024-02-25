d7cb9f918acde1ce64f1a137fbd47cae12d84f2c
danielsidorowicz <daniel.sidorowicz05@gmail.com>
2024-02-25 23:55:07 +0100

    po feriach >:(

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ import Location from "./src/modules/location.js";
 
 
 let LocationArray = []
-let locationId = '47' // 47 = start
+let locationId = '11' // 47 = start
 let allowedDirections = []
+let allowedDirectionsAll = [
+    "N", "North", "n", "north",
+    "S", "South", "s", "south",
+    "W", "West", "w", "west",
+    "E", "East", "e", "east"
+]
+let gameItems = []
+let locationItems = []
+let carriedItem = [
+    {
+        "id": 24,
+        "displayText": "a SPADE",
+        "usageFlag": 1,
+        "itemName": "SPADE"
+    }
+]
+let dependencies = []
+let okCounter = 0
+
+let dragonKilled = false
 
 let showGame = true
 let showGossip = false
@@ -18,15 +38,21 @@ function pressAnyKey() {
     showVocabulary = false
     document.getElementById("gossip").style.display = 'none'
     showGossip = false
-    document.body.removeEventListener("keypress", pressAnyKey)
+    document.body.removeEventListener("keydown", pressAnyKey)
+    input.value = ""
+    input.focus()
 }
 
 
 input.addEventListener("keypress", function (event) {
-    if (showGame) {
-        let directionTransitionText = "You are going "
-        if (event.key === "Enter") {
-            let playerInput = input.value
+
+    let directionTransitionText = "You are going "
+    if (event.key === "Enter") {
+        let playerInput = input.value
+
+        let playerInputSplit = playerInput.split(" ")
+
+        if (allowedDirectionsAll.includes(playerInput)) {
             if (allowedDirections.includes(playerInput)) {
                 input.disabled = true
                 let locationIdSplit = locationId.split('')
@@ -76,35 +102,182 @@ input.addEventListener("keypress", function (event) {
                         }
                     });
                 }
-                input.value = ""
-            } else if (playerInput == "V" || playerInput == "Vocabulary" || playerInput == "v" || playerInput == "vocabulary") {
-                document.getElementById("gameStuff").style.display = 'none'
-                showGame = false
-                document.getElementById("vocabulary").style.display = 'flex'
-                showVocabulary = true
-                document.getElementById("gossip").style.display = 'none'
-                showGossip = false
-                document.body.addEventListener("keypress", pressAnyKey)
-            } else if (playerInput == "G" || playerInput == "Gossip" || playerInput == "g" || playerInput == "gossip") {
-                document.getElementById("gameStuff").style.display = 'none'
-                showGame = false
-                document.getElementById("vocabulary").style.display = 'none'
-                showVocabulary = false
-                document.getElementById("gossip").style.display = 'flex'
-                showGossip = true
-                document.body.addEventListener("keypress", pressAnyKey)
             } else {
-                input.value = ""
                 document.getElementById('gameInputText').innerText = "You can't go that way"
             }
+            input.value = ""
+        } else if (playerInput == "V" || playerInput == "Vocabulary" || playerInput == "v" || playerInput == "vocabulary") {
+            document.getElementById("gameStuff").style.display = 'none'
+            showGame = false
+            document.getElementById("vocabulary").style.display = 'flex'
+            showVocabulary = true
+            document.getElementById("gossip").style.display = 'none'
+            showGossip = false
+            document.body.addEventListener("keydown", pressAnyKey)
+        } else if (playerInput == "G" || playerInput == "Gossip" || playerInput == "g" || playerInput == "gossip") {
+            document.getElementById("gameStuff").style.display = 'none'
+            showGame = false
+            document.getElementById("vocabulary").style.display = 'none'
+            showVocabulary = false
+            document.getElementById("gossip").style.display = 'flex'
+            showGossip = true
+            document.body.addEventListener("keydown", pressAnyKey)
+        } else if (playerInputSplit[0] == "TAKE" || playerInputSplit[0] == "T" || playerInputSplit[0] == "take" || playerInputSplit[0] == "t") {
+            let wantedItem = playerInputSplit[1]
+            if (locationItems.length != 0) {
+                locationItems.find((item, idx) => {
+                    if (item.itemName == wantedItem) {
+                        if (carriedItem.length == 0) {
+                            gameItems.find((element) => {
+                                if (element.itemName == wantedItem) {
+                                    if (element.usageFlag == 1) {
+                                        let takingTransitionText = `You are taking ${element.displayText}`
+                                        carriedItem.push(element)
+                                        locationItems.splice(idx, 1)
+                                        LocationArray.find((element) => {
+                                            if (element.id == locationId) {
+                                                updateGame(element, takingTransitionText)
+                                            }
+                                        });
+                                    } else {
+                                        document.getElementById('gameInputText').innerText = "You can't carry it"
+                                    }
+                                }
+                            });
 
+                        } else {
+                            document.getElementById('gameInputText').innerText = "You are carrying something"
+                        }
+                    } else {
+                        document.getElementById('gameInputText').innerText = "There isn't anything like that here"
+                    }
+                })
+            } else {
+                document.getElementById('gameInputText').innerText = "There isn't anything like that here"
+            }
+
+            input.value = ""
+        } else if (playerInputSplit[0] == "DROP" || playerInputSplit[0] == "D" || playerInputSplit[0] == "drop" || playerInputSplit[0] == "d") {
+            let wantedItem = playerInputSplit[1]
+            if (carriedItem[0]) {
+                if (carriedItem[0].itemName == wantedItem) {
+                    if (locationItems.length < 3) {
+                        locationItems.push(carriedItem[0])
+                        let droppingTransitionText = `You are about to drop ${carriedItem[0].displayText}`
+                        carriedItem = []
+                        LocationArray.find((element) => {
+                            if (element.id == locationId) {
+                                updateGame(element, droppingTransitionText)
+                            }
+                        });
+                    } else {
+                        document.getElementById('gameInputText').innerText = "You can't store any more here"
+                    }
+                } else {
+                    document.getElementById('gameInputText').innerText = "You are not carrying it"
+                }
+            } else {
+                document.getElementById('gameInputText').innerText = "You are not carrying anything"
+            }
+
+            input.value = ""
+        } else if (playerInputSplit[0] == "USE" || playerInputSplit[0] == "U" || playerInputSplit[0] == "use" || playerInputSplit[0] == "u") {
+            if (playerInputSplit[1]) {
+                let wantedItem = playerInputSplit[1]
+                if (carriedItem[0].itemName == wantedItem) {
+                    gameItems.find((element) => {
+                        if (element.itemName == wantedItem) {
+                            dependencies.find((depends) => {
+                                if (depends.itemID == element.id) {
+                                    if (depends.locationID == locationId) {
+                                        gameItems.find((give) => {
+                                            if (give.id == depends.resultID) {
+                                                carriedItem = []
+                                                if (depends.staysAtLocation == 0) {
+                                                    carriedItem.push(give)
+                                                } else {
+                                                    locationItems.push(give)
+                                                }
+
+                                                if (give.important == "OK") {
+                                                    okCounter++
+                                                }
+                                                let usingTransitionText = depends.message
+                                                LocationArray.find((element) => {
+                                                    if (element.id == locationId) {
+                                                        updateGame(element, usingTransitionText)
+                                                    }
+                                                });
+                                            }
+                                        })
+                                    } else {
+                                        document.getElementById('gameInputText').innerText = "Nothing happened"
+                                    }
+                                }
+                            })
+                        }
+                    })
+                } else {
+                    document.getElementById('gameInputText').innerText = "You aren't carrying anything like that"
+                }
+            } else {
+                document.getElementById('gameInputText').innerText = "You aren't carrying anything like that"
+            }
+            input.value = ""
+        } else {
+            input.value = ""
+            document.getElementById('gameInputText').innerText = "Try another word or V for vocabulary"
         }
+
     }
+
 })
 
-function updateGame(element, directionTransitionText) {
-    document.getElementById('gameInputText').innerText = directionTransitionText
+let counter = 0
+
+function sleep() {
+    return new Promise((r) => setTimeout(r, ms));
+}
+
+async function displayTextArray(Text) {
+    if (counter == 0) {
+        document.getElementById('gameInputText').innerText = Text[counter]
+        counter++;
+        displayTextArray(Text)
+    } else {
+        setTimeout(function () {
+            document.getElementById('gameInputText').innerText = Text[counter]
+            counter++;
+            if (counter < Text.length) {
+                displayTextArray(Text)
+            }
+        }, 2000)
+    }
+}
+
+function updateGameSpecifics(element) {
     setTimeout(() => {
+        if (carriedItem.length == 0) {
+            document.getElementById("gameCarryText").innerText = `You are carrying nothing`
+        } else {
+            document.getElementById("gameCarryText").innerText = `You are carrying ${carriedItem[0].displayText}`
+        }
+
+        locationItems = element.items
+        if (locationItems.length != 0) {
+            let showTextString = "You see "
+            for (let i = 0; i < locationItems.length; i++) {
+                showTextString += locationItems[i].displayText
+                if (i != locationItems.length - 1) {
+                    showTextString += ", "
+                }
+            }
+            document.getElementById('gameObjectsText').innerText = showTextString
+        } else {
+            document.getElementById('gameObjectsText').innerText = "You see nothing"
+        }
+
+
         allowedDirections = []
         let textToGo = 'You can go '
         document.getElementById('imgGameShow').src = `./src/img/${element.imgSrc}`
@@ -147,14 +320,28 @@ function updateGame(element, directionTransitionText) {
         input.focus()
         document.getElementById('gameInputText').innerText = "What now?"
     }, 500)
+}
+
+function updateGame(element, TransitionText) {
+    input.value = ""
+    if (Array.isArray(TransitionText)) {
+        counter = 0
+        displayTextArray(TransitionText).then(() => { updateGameSpecifics(element) })
+    } else {
+        document.getElementById('gameInputText').innerText = TransitionText
+        updateGameSpecifics(element)
+    }
 
 }
 
 fetch("./src/data/data.json")
     .then(response => response.json())
     .then(json => {
+        gameItems = json.items
+        dependencies = json.dependencies
+        console.log(dependencies)
         for (let i = 0; i < json.map.length; i++) {
-            let locat = new Location(json.map[i].id, json.map[i].text, json.map[i].imgSrc, json.map[i].bgColor, json.map[i].directions)
+            let locat = new Location(json.map[i].id, json.map[i].text, json.map[i].imgSrc, json.map[i].bgColor, json.map[i].directions, json.map[i].items)
             LocationArray.push(locat)
         }
         console.log(LocationArray);
--- src/modules/location.js
+++ src/modules/location.js
@@ -1,9 +1,10 @@
 export default class Location {
-    constructor(id, text, imgSrc, bgColor, directions) {
+    constructor(id, text, imgSrc, bgColor, directions, items) {
         this.id = id
         this.text = text
         this.imgSrc = imgSrc
         this.bgColor = bgColor
         this.directions = directions
+        this.items = items
     }
 }
